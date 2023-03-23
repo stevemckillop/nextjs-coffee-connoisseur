2d47e7b3d5e1c0c0b4c9c8890d6de96356205499
stevemckillop <92012639+stevemckillop@users.noreply.github.com>
2023-03-23 13:21:42 +0900

    feat: add dynamic routing - video 40

---
+++ src/pages/coffee-store/[id].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+
+const CoffeeStore = () => {
+  const router = useRouter()
+  console.log("router", router);
+  return <div>Coffee Store Page {router.query.id}</div>
+}
+
+export default CoffeeStore;
