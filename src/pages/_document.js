6ebf60e80bcb661501b57144f90b0622fdcb057a
stevemckillop <92012639+stevemckillop@users.noreply.github.com>
2023-03-24 23:04:24 +0900

    feat: apply fonts in Document - vid 56

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/NotoSans-Bold.ttf"
+          as='font'
+          crossOrigin='anonymous'>
+        </link>
+        <link
+          rel="preload"
+          href="/fonts/NotoSans-Regular.ttf"
+          as='font'
+          crossOrigin='anonymous'>
+        </link>
+        <link
+          rel="preload"
+          href="/fonts/NotoSans-SemiBold.ttf"
+          as='font'
+          crossOrigin='anonymous'>
+        </link>
+      </Head>
       <body>
         <Main />
         <NextScript />
